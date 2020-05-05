d90bfb0c1fa31e3082ca710ee6fc72066c102969
AntonIsaksson <anton.isak@outlook.com>
2020-05-05 17:39:51 +0200

    Added env.py

---
+++ django_todo/django_todo/env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault("SECRET_KEY", "secret")
+os.environ.setdefault("DATABASE_URL", "postgres://gckrgjkfyzxlyx:a2d7ad4dab53064a8d2c8e79086ca2fe0a2553b48543ca5acb657e35a36f59cf@ec2-54-247-94-127.eu-west-1.compute.amazonaws.com:5432/d6tmtbjll550j0")
--- django_todo/django_todo/settings.py
+++ django_todo/django_todo/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # }
 
 DATABASES = {'default': dj_database_url.parse("postgres://gckrgjkfyzxlyx:a2d7ad4dab53064a8d2c8e79086ca2fe0a2553b48543ca5acb657e35a36f59cf@ec2-54-247-94-127.eu-west-1.compute.amazonaws.com:5432/d6tmtbjll550j0")}
-
+#DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
